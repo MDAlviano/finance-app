4f56161c48a3e118aef2edd4798c32d938e4ab00
MDAlviano <mdalviano.21122@gmail.comgit initgit config --global user.email mdalviano.21122@gmail.com>
2024-08-27 20:46:48 +0700

    first bang

---
+++ app/src/main/java/com/alviano/app/finance/IntroActivity.kt
@@ -0,0 +1,33 @@
+package com.alviano.app.finance
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.alviano.app.finance.databinding.ActivityIntroBinding
+
+class IntroActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityIntroBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+
+        binding.startBtn.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+    }
+}
