f5eb8d4854ac2201d8683ff46db6a6f5d2d79e59
MDAlviano <mdalviano.21122@gmail.com>
2024-09-18 21:24:17 +0700

    membuat file activity_report dan membuat CircularProgressBar

--- app/src/main/java/com/alviano/app/finance/Repository/MainRepository.kt
+++ app/src/main/java/com/alviano/app/finance/Repository/MainRepository.kt
@@ -9,6 +9,9 @@ class MainRepository {
         ExpenseDomain("Mcdonald", 77.82, "img2", "15 jun 2024 21:54"),
         ExpenseDomain("Cinema", 23.47, "img3", "14 jun 2024 07:39"),
         ExpenseDomain("Restaurant", 341.12, "img1", "12 jun 2024 12:46"),
+        ExpenseDomain("Restaurant", 341.12, "img1", "12 jun 2024 12:46"),
+        ExpenseDomain("Restaurant", 341.12, "img1", "12 jun 2024 12:46"),
+        ExpenseDomain("Restaurant", 341.12, "img1", "12 jun 2024 12:46"),
     )
 
 }
